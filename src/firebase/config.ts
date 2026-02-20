07ff6e0f40605eebd4987b6756dcc4b826c71dbd
Controle Escolar <controle.escolar.sb@gmail.com>
2026-02-20 14:13:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7340493758-f1f41",
+  "appId": "1:871091404847:web:c17b578192c96f9d2e06da",
+  "apiKey": "AIzaSyDUrloNOOQ6EDS_XuJIjinP14EBAHLreLA",
+  "authDomain": "studio-7340493758-f1f41.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "871091404847"
+};
